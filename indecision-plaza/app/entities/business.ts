e3b28beeeb03464d3d84fb51396e3d94a50028bb
Luke Shadler <lshadler13@gmail.com>
2017-11-03 19:15:02 -0400

    Refactor business data type

---
+++ indecision-plaza/app/entities/business.ts
@@ -0,0 +1,43 @@
+export class Business {
+
+  private static readonly METERS_IN_MILE = 1609.34;
+  /** Name of the business. */
+  public name: string;
+  /** Phone number for the business. */
+  public phoneNumber: string;
+  /** Distance from the current location. */
+  public distance: string;
+  /** URL Image for business. */
+  public imageUrl: string;
+  /** First line of the business address. */
+  public basicAddress: string;
+  /** Business rating. */
+  public rating: number;
+  /** Price of business. */
+  public price: string;
+
+  public constructor(json) {
+    this.name = json.name;
+    this.phoneNumber = json.display_phone;
+    this.distance = (json.distance/Business.METERS_IN_MILE).toFixed(1);
+    this.imageUrl = json.image_url;
+    this.basicAddress = json.location.address1;
+    this.rating = json.rating;
+    this.price = json.price;
+  }
+
+  public toLongString() {
+    return this.name + "\n"
+        + "\t" + this.basicAddress + "\n"
+        + "\tDistance: " + this.distance + " miles\n"
+        + "\tRating: " + this.rating + "\n"
+        + "\tPrice: " + this.price + "\n"
+        + "\tPhone: " + this.phoneNumber
+  }
+
+  public toShortString() {
+    return this.name + "\n"
+        + "\t" + this.basicAddress + "\n"
+        + "\tDistance: " + this.distance + " meters\n"
+  }
+}
--- indecision-plaza/app/food-finder/food-finder.component.ts
+++ indecision-plaza/app/food-finder/food-finder.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from "@angular/core";
 import { FinderService } from "../services/finder.service";
 import { Location, isEnabled, enableLocationRequest, getCurrentLocation, watchLocation, distance, clearWatch } from "nativescript-geolocation";
-
+import { Business } from "../entities/business";
 @Component({
     selector: "food-finder",
-    styleUrls: ["./food-finder.component.css"],
-    templateUrl: "./food-finder.component.html",
+    styleUrls: ["./food-finder/food-finder.component.css"],
+    templateUrl: "./food-finder/food-finder.component.html",
 })
 export class FoodFinderComponent {
   public foodInfo: string = "";
-  public chosenPlace;
+  public chosenPlace: Business;
   public currentLocation: Location;
   constructor(private foodSvc: FinderService) {
     enableLocationRequest();
@@ -18,7 +18,7 @@ export class FoodFinderComponent {
   findFood(){
     this.foodSvc.getNearbyFood().forEach((data) => {
       let places = data.businesses
-      this.chosenPlace = places[Math.floor(Math.random()*places.length)]
+      this.chosenPlace = new Business(places[Math.floor(Math.random()*places.length)]);
       if(this.chosenPlace === undefined || this.chosenPlace === null)
         this.foodInfo = "No food places in your area";
       else
@@ -27,12 +27,8 @@ export class FoodFinderComponent {
 
   }
 
-  setInfo(business): void {
-    this.foodInfo = business.name + "\n"
-        + "\t" + business.location.address1 + "\n"
-        + "\t" + business.location.city + ", " + business.location.state + "\n"
-        + "\tRating: " + business.rating + "\n"
-        + "\tPrice: " + business.price;
+  setInfo(business: Business): void {
+    this.foodInfo = business.toLongString();
 
   }
 
