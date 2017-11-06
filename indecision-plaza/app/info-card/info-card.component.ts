2478cc89d1a5650bc4c807c9ac32221f262b7235
Luke Shadler <lshadler13@gmail.com>
2017-11-06 09:34:50 -0500

    change pathing to account for tns bug

--- indecision-plaza/app/info-card/info-card.component.ts
+++ indecision-plaza/app/info-card/info-card.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit, Input } from "@angular/core";
 import { Business } from "../entities/business";
 @Component({
     selector: "info-card",
-    styleUrls: ["./info-card/info-card.component.css"],
-    templateUrl: "./info-card/info-card.component.html",
+    styleUrls: ["./info-card.component.css"],
+    templateUrl: "./info-card.component.html",
 })
 export class InfoCardComponent {
   @Input() business: Business;
