ed13e27118f605eb7676df5d765b505f64832652
Luke Shadler <lshadler13@gmail.com>
2017-11-14 16:01:23 -0500

    Implement website linking

--- indecision-plaza/app/info-card/info-card.component.ts
+++ indecision-plaza/app/info-card/info-card.component.ts
@@ -1,17 +1,38 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Business } from "../entities/business";
+import * as TNSPermissions from "nativescript-permissions"
 import * as TNSPhone from "nativescript-phone"
+import * as TNSUtils from "utils/utils";
+declare var android;
+
 @Component({
     selector: "info-card",
-    styleUrls: ["./info-card.component.css"],
-    templateUrl: "./info-card.component.html",
+    styleUrls: ["./info-card/info-card.component.css"],
+    templateUrl: "./info-card/info-card.component.html",
 })
 export class InfoCardComponent {
   @Input() business: Business;
+  public buttonText: string;
 
-  constructor() {}
+  constructor() {
+  }
+
+  ngOnChanges() {
+    this.buttonText = this.business != null && this.business != undefined ? "Website" : "";
+  }
 
   dialNumber() {
-    TNSPhone.dial(this.business.phoneNumber,false)
+    TNSPermissions.requestPermission(android.Manifest.permission.CALL_PHONE,
+                                    "App Needs This Permission To Make Phone Calls")
+    .then(() =>{
+      TNSPhone.dial(this.business.phoneNumber,false)
+    })
+    .catch(() =>{
+      console.log("Permission Denied.")
+    })
+  }
+
+  openWebsite() {
+    TNSUtils.openUrl(this.business.yelpURL);
   }
 }
