72a4b64679a6232c9d9422102287c2d70d976aa3
Luke Shadler <lshadler13@gmail.com>
2017-11-14 16:08:47 -0500

    remove temporary definitions

--- indecision-plaza/app/info-card/info-card.component.ts
+++ indecision-plaza/app/info-card/info-card.component.ts
@@ -7,8 +7,8 @@ declare var android;
 
 @Component({
     selector: "info-card",
-    styleUrls: ["./info-card/info-card.component.css"],
-    templateUrl: "./info-card/info-card.component.html",
+    styleUrls: ["./info-card.component.css"],
+    templateUrl: "./info-card.component.html",
 })
 export class InfoCardComponent {
   @Input() business: Business;
