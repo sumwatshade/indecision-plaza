7b07aed45ec74b12bd1b53dadd38756eeae2cc06
Luke Shadler <lshadler13@gmail.com>
2017-11-13 20:07:25 -0500

    Implement dial functionality

--- indecision-plaza/app/info-card/info-card.component.ts
+++ indecision-plaza/app/info-card/info-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Business } from "../entities/business";
+import * as TNSPhone from "nativescript-phone"
 @Component({
     selector: "info-card",
     styleUrls: ["./info-card.component.css"],
@@ -9,4 +10,8 @@ export class InfoCardComponent {
   @Input() business: Business;
 
   constructor() {}
+
+  dialNumber() {
+    TNSPhone.dial(this.business.phoneNumber,false)
+  }
 }
