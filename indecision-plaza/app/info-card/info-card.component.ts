ab820e028e033c5d7e3a12c7ae4f7b71732105f1
Luke Shadler <lshadler13@gmail.com>
2017-11-16 23:22:30 -0500

    refactor info card to accept new picker

--- indecision-plaza/app/info-card/info-card.component.ts
+++ indecision-plaza/app/info-card/info-card.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Business } from "../entities/business";
-import * as TNSPermissions from "nativescript-permissions"
-import * as TNSPhone from "nativescript-phone"
 import * as TNSUtils from "utils/utils";
 declare var android;
 
 @Component({
   selector: "info-card",
-  styleUrls: ["./info-card.component.css"],
-  templateUrl: "./info-card.component.html",
+  styleUrls: ["./info-card/info-card.component.css"],
+  templateUrl: "./info-card/info-card.component.html",
 })
 export class InfoCardComponent {
   @Input() business: Business;
@@ -19,17 +17,6 @@ export class InfoCardComponent {
 
   ngOnChanges() {
     this.buttonText = this.business != null && this.business != undefined ? "Website" : "";
-  }
-
-  dialNumber() {
-    TNSPermissions.requestPermission(android.Manifest.permission.CALL_PHONE,
-                                    "App Needs This Permission To Make Phone Calls")
-    .then(() =>{
-      TNSPhone.dial(this.business.phoneNumber,false)
-    })
-    .catch(() =>{
-      console.log("Permission Denied.")
-    })
   }
 
   openWebsite() {
