931e929954cca7ea3db600b192c5ff38c11379fb
Luke Shadler <lshadler13@gmail.com>
2017-11-16 23:21:05 -0500

    Implement list finder

--- indecision-plaza/app/food-finder/food-finder.component.ts
+++ indecision-plaza/app/food-finder/food-finder.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from "@angular/core";
 import { FinderService } from "../services/finder.service";
 import { Location, isEnabled, enableLocationRequest, getCurrentLocation, watchLocation, distance, clearWatch } from "nativescript-geolocation";
 import { Business } from "../entities/business";
+import { ListPicker } from "ui/list-picker";
 @Component({
   selector: "food-finder",
-  styleUrls: ["./food-finder.component.css"],
-  templateUrl: "./food-finder.component.html",
+  styleUrls: ["./food-finder/food-finder.component.css"],
+  templateUrl: "./food-finder/food-finder.component.html",
 })
 export class FoodFinderComponent {
   public foodInfo: string = "";
@@ -17,22 +18,32 @@ export class FoodFinderComponent {
   public chosenCategory: string;
   constructor(private foodSvc: FinderService) {
     enableLocationRequest();
-    this.categories = ["Restaraunts","Bars","Coffee/Tea","Breakfast"];
+    this.chosenPlace = Business.makeEmpty();
+    this.categories = ["Bars","Coffee/Tea","Restaraunts","Breakfast","Fast Food"];
     this.categoryMap["Restaraunts"] = "restaraunts";
     this.categoryMap["Bars"] = "bars";
     this.categoryMap["Coffee/Tea"] = "coffee";
     this.categoryMap["Breakfast"] = "breakfast_brunch";
+    this.categoryMap["Fast Food"] = "hotdogs";
 
   }
 
   findFood() {
-    this.foodSvc.getNearbyFood("restaraunts").forEach((data) => {
+    this.foodSvc.getNearbyFood(this.categoryMap[this.chosenCategory]).forEach((data) => {
       // Grab the list of businesses from the JSON
       let places = data.businesses
-      // Choose one random business
-      let chosenPlaceJSON = places[Math.floor(Math.random() * places.length)]
-      // Convert chosen business into an Entity that is read into the info card
-      this.chosenPlace = new Business(chosenPlaceJSON);
+
+      if(places.length !== 0) {
+        // Choose one random business
+        let chosenPlaceJSON = places[Math.floor(Math.random() * places.length)]
+        // Convert chosen business into an Entity that is read into the info card
+        this.chosenPlace = new Business(chosenPlaceJSON);
+        console.log(this.chosenPlace.toLongString())
+      }
+      else {
+        this.chosenPlace = Business.makeEmpty();
+        console.log("No results were found");
+      }
 
       // Handles the case that no food places are found
       // TODO: Implement a Utility that can perform these checks as one function
@@ -42,6 +53,11 @@ export class FoodFinderComponent {
         this.setInfo(this.chosenPlace);
     });
 
+  }
+
+  public selectedIndexChanged(args) {
+      let picker = <ListPicker>args.object;
+      this.chosenCategory = this.categories[picker.selectedIndex];
   }
 
   setInfo(business: Business): void {
